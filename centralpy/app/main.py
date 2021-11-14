19e6c30d40b3b7ba4a36ca14a1c83c5d435abff9
studioj <22102283+studioj@users.noreply.github.com>
2021-11-14 12:30:08 +0100

    adding database visibility adding location sensor type

--- centralpy/app/main.py
+++ centralpy/app/main.py
@@ -5,7 +5,6 @@ import pathlib
 import uvicorn
 from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from db import SessionLocal, engine
 from pydantic import BaseModel
@@ -14,6 +13,18 @@ from models import Measurement, Base
 
 class TempSensReport(BaseModel):
     value: float
+    time: datetime.datetime
+    sensor_type: str
+    unit: str
+    location: str
+
+
+class CO2SensReport(BaseModel):
+    value: float
+    time: datetime.datetime
+    sensor_type: str
+    unit: str
+    location: str
 
 
 def get_db():
@@ -33,21 +44,48 @@ templates_dir = Jinja2Templates(directory=os.path.join(this_file_dir, "templates
 
 
 @app.get("/")
-async def home(request: Request):
-    data = {"text": "hi"}
-    return templates_dir.TemplateResponse("dashboard.html", {"request": request})
+async def home(request: Request, db: Session = Depends(get_db)):
+    measurements = db.query(Measurement).all()
+    return templates_dir.TemplateResponse("dashboard.html", {"request": request, "measurements": measurements})
 
 
 @app.post("/api/temperature/")
-async def temperature_measurement(temp_report: TempSensReport, background_tasks=BackgroundTasks, db: Session = Depends(get_db)):
+async def temperature_measurement(temp_report: TempSensReport, db: Session = Depends(get_db)):
     """
     api to post measurements to
 
-    :param temp_report:
+    :param temp_report: post temperature in centi degrees (27.6 degrees Celsius becomes 2760)
+    :param db:
     :return:
     """
     measurement = Measurement()
     measurement.value = temp_report.value
+    measurement.time = temp_report.time
+    measurement.sensor = temp_report.sensor_type
+    measurement.unit = temp_report.unit
+    measurement.location = temp_report.location
+    measurement.type = "temperature"
+    db.add(measurement)
+    db.commit()
+    return {"data": "data"}
+
+
+@app.post("/api/co2/")
+async def temperature_measurement(co2_report: CO2SensReport, db: Session = Depends(get_db)):
+    """
+    api to post measurements to
+
+    :param co2_report: post co2 in ppm
+    :param db:
+    :return:
+    """
+    measurement = Measurement()
+    measurement.value = co2_report.value
+    measurement.time = co2_report.time
+    measurement.sensor = co2_report.sensor_type
+    measurement.unit = co2_report.unit
+    measurement.location = co2_report.location
+    measurement.type = "co2"
     db.add(measurement)
     db.commit()
     return {"data": "data"}
--- centralpy/app/models.py
+++ centralpy/app/models.py
@@ -9,4 +9,8 @@ class Measurement(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     time = Column(DateTime, index=True)
+    unit = Column(String(30), nullable=False, index=True)
+    type = Column(String(30), nullable=False, index=True)
+    sensor = Column(String(30), nullable=False, index=True)
+    location = Column(String(100), nullable=False, index=True)
     value = Column(Numeric(3, 2))
